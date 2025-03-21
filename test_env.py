e3504292e2b4119e224bf47a81c3bc0972361ea8
tonkatusan70 <yuki.xox711@gmail.com>
2025-03-21 17:01:25 +0900

    Create test_env.py

---
+++ test_env.py
@@ -0,0 +1,5 @@
+import os
+
+print("GOOGLE_SEARCH_API_KEY:", os.getenv("GOOGLE_SEARCH_API_KEY"))
+print("GOOGLE_SEARCH_CX:", os.getenv("GOOGLE_SEARCH_CX"))
+print("GEMINI_API_KEY:", os.getenv("GEMINI_API_KEY"))
